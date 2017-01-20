1b817ffe5274891613f565c543dc798f6d36f8f6
David Kent <davidkent@fastmail.com.au>
2017-01-21 08:46:25 +1100

    Rework wifi connection logic to allow reconnect.
    This is to avoid https://github.com/micropython/micropython/issues/2547,
    caling connect results in flash write which will eventually wear out
    the flash. To avoid we let it try and connect automatically then
    fall back to manual call to connect.

--- boot.py
+++ boot.py
@@ -1,5 +1,6 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 
+import network
 import json
 config = json.load(open('config.json'))
 
@@ -13,29 +14,36 @@ if config.get('webrepl', True):
     webrepl.start()
     gc.collect()
 
+def wait_for_connection(sta_if):
+    import utime
+    while True:
+        status = sta_if.status()
+        if status == network.STAT_CONNECTING:
+            pass
+        elif status == network.STAT_GOT_IP:
+            print('network config:', sta_if.ifconfig())
+            return True
+        else:
+            # failed
+            print('unable to connect to network')
+            return False
+        utime.sleep_us(100)
+
 def do_connect():
-    import network
     try:
         ssid = config['wifi']["ssid"]
         password = config['wifi']["password"]
     except KeyError:
         print('failed to read config.json')
         return
+
     sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
+    if not wait_for_connection(sta_if):
         print('connecting to network...')
         sta_if.active(True)
         sta_if.connect(ssid, password)
-        while True:
-            status = sta_if.status()
-            if status == network.STAT_CONNECTING:
-                pass
-            elif status == network.STAT_GOT_IP:
-                print('network config:', sta_if.ifconfig())
-                break
-            else:
-                # failed
-                print('unable to connect to network')
-                break
+        wait_for_connection(sta_if)
+    else:
+        print('automatic reconnect successful')
 
 do_connect()
